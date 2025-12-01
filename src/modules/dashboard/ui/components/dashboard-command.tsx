bdce3d32537ce9e4be0dd429ca7d389673436c43
Deniz <denizaskar05@gmail.com>
2025-12-01 23:38:49 +0300

    feat: implement tRPC API and integrate with React Query for data fetching

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -11,13 +11,13 @@ export const DashboardCommand =({open, setOpen}:Props)=>{
         <CommandDialog open={open} onOpenChange={setOpen}>
             <CommandInput
             placeholder="Find a meeting or agent"
-            >
+            />
                 <CommandList>
                     <CommandItem>
                         Test
                     </CommandItem>
                 </CommandList>
-            </CommandInput>
+         
         </CommandDialog>
     )
 }
