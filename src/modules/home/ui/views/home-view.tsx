04d8e666004033d7af89ee237533f8831342af1c
Deniz <denizaskar05@gmail.com>
2025-12-02 23:43:07 +0300

    feat: add nanoid for unique ID generation and implement agents management

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,26 +1,11 @@
 "use client"
-import { Button } from '@/components/ui/button';
-import { authClient } from '@/lib/auth-client';
-import { useRouter } from 'next/navigation';
-export const HomeView = () => {
-  const router = useRouter();
-  const {data:session} = authClient.useSession();
-  if(!session){
-    return(
-      <p>Loading...</p>
-    )
-  }
+
+export const HomeView = () =>{
+
   return(
-    <div className='flex flex-col p-4 gap-y-4'>
-    <p>Logged in as {session.user.name}</p>
-    <Button onClick={() => authClient.signOut({
-        fetchOptions:{
-            onSuccess: () => router.push("/sign-in")
-        }
-    })}> 
-      Sign Out
-    </Button>
-  </div>
+    <div >
+      Home view
+    </div>
   )
 }
 
