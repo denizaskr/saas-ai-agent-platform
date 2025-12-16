e4e238ce8868e0367d2197a465df8e5a6c97c5a0
Deniz <denizaskar05@gmail.com>
2025-12-16 21:16:59 +0300

    feat: add pagination constants and implement filters for agent search parameters

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import {Inter} from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "@/components/ui/sonner";
-
+import {NuqsAdapter} from "nuqs/adapters/next";
 const inter = Inter({
   subsets: ["latin"],
 });
@@ -19,7 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TRPCReactProvider>
+   <NuqsAdapter>
+   <TRPCReactProvider>
       <html lang="en">
       <body
         className={`${inter.className}  antialiased`}
@@ -29,6 +30,8 @@ export default function RootLayout({
       </body>
     </html>
     </TRPCReactProvider>
+    </NuqsAdapter>
+    
     
   );
 }
--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,12 +1,25 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { PlusIcon } from "lucide-react"
+import { PlusIcon, X, XCircleIcon } from "lucide-react"
 import { useState } from "react";
 import { NewAgentDialog } from "./new-agent-dialog";
+import { useAgentFilters } from "../../hooks/use-agents-filters";
+import { AgentsSearchFilter } from "./agents-search-filter";
+import { DEFAULT_PAGE } from "@/constants";
 
 export const AgentsListHeader = () => { 
+    const [filters, setFilters] = useAgentFilters();
     const [isDialogOpen, setIsDialogOpen] = useState(false);
+    
+    const isAnyFiltterModified= !!filters.search;
+
+    const onClearFilters = ()=>{
+        setFilters({
+            search:"",
+            page:DEFAULT_PAGE,
+        })
+    }
     return (
         <>
         <NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}/>
@@ -18,7 +31,16 @@ export const AgentsListHeader = () => {
                 New Agent
             </Button>
            </div>
-        </div>@
+        </div>
+        <div className="flex items-center gap-x-2 p-1">
+        <AgentsSearchFilter/>
+        {isAnyFiltterModified && (
+            <Button variant="outline" size="sm" onClick={onClearFilters}>
+            <XCircleIcon/>
+            Clear
+            </Button>
+        )}
+        </div>
         </>
         
     )
